0147dbf878b219c7b0ef739c3543ac012bfebdc3
”qinjuncai“ <qinjuncaicn@gmail.com>
2018-04-19 17:50:08 +0800

    springmvc + spring + mybatis + jsp

---
+++ src/main/java/com/rzhd/service/BaseService.java
@@ -0,0 +1,15 @@
+package com.rzhd.service;
+
+
+/**
+  *@author 胡佳奇
+  *
+  *@description: 基础的service类
+  *
+  *@date:Create in  2018/4/10_15:55
+  *
+  *@modify By:
+ */ 
+public class BaseService<T> {
+
+}
